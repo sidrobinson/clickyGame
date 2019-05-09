cc005108db4ff7bc008771683d359b79f9d53838
Bernard Robinson <bernardrobinson@Bernards-Air.attlocal.net>
2019-05-08 20:30:02 -0400

    update

--- card-match-up/src/components/cards/GameOver.jsx
+++ card-match-up/src/components/cards/GameOver.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const GameOver = ({ restartGame }) => (
   <div className="justify-center">
     <h1>Game Over!</h1>
-    <h3>If you enjoyed playing this game, follow me @iamkenec for more...</h3>
     <button className="restart-button" onClick={restartGame}>Restart Game</button>
   </div>
 );
